a65517300de25831af9f4c663f7c2ff1890a3c4b
Bela Kiss <bela_kiss@epam.com>
2021-04-21 18:21:11 +0200

    Demo v1 update

---
+++ docs/demo_v1/viewer/story.js
@@ -0,0 +1,657 @@
+var story = {
+ "docName": "demo",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "81A3D74B-4C31-4B42-87E9-51C470AD1C2A",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 0,
+   "image": "dashboard.png",
+   "image2x": "dashboard@2x.png",
+   "width": 1440,
+   "height": 1362,
+   "x": -1051,
+   "y": -4889,
+   "title": "Dashboard",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": 0,
+     "y": 0,
+     "width": 1440,
+     "height": 237,
+     "type": "float",
+     "index": 0,
+     "isFloat": true,
+     "isFixedDiv": false,
+     "divID": "",
+     "links": [
+      {
+       "rect": {
+        "x": 300,
+        "y": 0,
+        "width": 100,
+        "height": 60
+       },
+       "isParentFixed": true,
+       "page": 1,
+       "index": 0
+      },
+      {
+       "rect": {
+        "x": 400,
+        "y": 0,
+        "width": 103,
+        "height": 60
+       },
+       "isParentFixed": true,
+       "page": 3,
+       "index": 1
+      },
+      {
+       "rect": {
+        "x": 503,
+        "y": 0,
+        "width": 101,
+        "height": 60
+       },
+       "isParentFixed": true,
+       "page": 5,
+       "index": 2
+      },
+      {
+       "rect": {
+        "x": 666,
+        "y": 195,
+        "width": 30,
+        "height": 30
+       },
+       "isParentFixed": true,
+       "page": 7,
+       "index": 3
+      },
+      {
+       "rect": {
+        "x": 666,
+        "y": 141,
+        "width": 30,
+        "height": 30
+       },
+       "isParentFixed": true,
+       "page": 10,
+       "index": 4
+      }
+     ],
+     "image": "dashboard-0.png",
+     "image2x": "dashboard-0@2x.png"
+    }
+   ],
+   "links": [
+    {
+     "rect": {
+      "x": 52,
+      "y": 423,
+      "width": 16,
+      "height": 16
+     },
+     "isParentFixed": false,
+     "page": 8,
+     "index": 5
+    },
+    {
+     "rect": {
+      "x": 666,
+      "y": 562,
+      "width": 30,
+      "height": 30
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 6
+    },
+    {
+     "rect": {
+      "x": 666,
+      "y": 953,
+      "width": 30,
+      "height": 30
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 7
+    }
+   ]
+  },
+  {
+   "id": "F5EAADD4-8074-4B35-A0B4-AEDDA8C6BCA1",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 1,
+   "image": "menu_3_subnav.png",
+   "image2x": "menu_3_subnav@2x.png",
+   "width": 160,
+   "height": 136,
+   "x": 485,
+   "y": -4866,
+   "title": "Menu 3 subnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 0,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": true,
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "F5B8A0A4-2982-4CF3-8146-A250E92B485D",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 2,
+   "image": "menu_4_subnav_1_subsubnav.png",
+   "image2x": "menu_4_subnav_1_subsubnav@2x.png",
+   "width": 220,
+   "height": 102,
+   "x": 685,
+   "y": -4700,
+   "title": "Menu 4 subnav 1 subsubnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 6,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "EDA0C24B-F375-4E16-A271-FABD5CF75C2B",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 3,
+   "image": "menu_4_subnav.png",
+   "image2x": "menu_4_subnav@2x.png",
+   "width": 160,
+   "height": 136,
+   "x": 485,
+   "y": -4700,
+   "title": "Menu 4 subnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 0,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 160,
+      "height": 34
+     },
+     "isParentFixed": false,
+     "page": 2,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 0,
+      "y": 68,
+      "width": 160,
+      "height": 34
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 1
+    }
+   ]
+  },
+  {
+   "id": "D8520B25-F36F-41C1-9A0D-898936119BD6",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 4,
+   "image": "menu_4_subnav_3_subsubnav.png",
+   "image2x": "menu_4_subnav_3_subsubnav@2x.png",
+   "width": 220,
+   "height": 68,
+   "x": 685,
+   "y": -4568,
+   "title": "Menu 4 subnav 3 subsubnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 6,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "91D0E8B0-9B5E-43D2-BD7C-786EA4FB3395",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 5,
+   "image": "menu_5_subnav.png",
+   "image2x": "menu_5_subnav@2x.png",
+   "width": 140,
+   "height": 68,
+   "x": 485,
+   "y": -4534,
+   "title": "Menu 5 subnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 0,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 34,
+      "width": 140,
+      "height": 34
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "FD87AB87-F765-4325-94AE-AD3959780106",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 6,
+   "image": "menu_5_subnav_2_subsubnav.png",
+   "image2x": "menu_5_subnav_2_subsubnav@2x.png",
+   "width": 220,
+   "height": 68,
+   "x": 685,
+   "y": -4470,
+   "title": "Menu 5 subnav 2 subsubnav",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 30,
+    "totalWidth": 1380,
+    "numberOfColumns": 12,
+    "columnWidth": 96.66666666666667,
+    "gutterWidth": 20
+   },
+   "type": "overlay",
+   "overlayByEvent": 1,
+   "overlayPin": 0,
+   "overlayPinHotspot": 6,
+   "overlayPinPage": 0,
+   "overlayOverFixed": true,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "16035A27-0291-42BE-A787-549BA5AAD3EB",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 7,
+   "image": "context_menu.png",
+   "image2x": "context_menu@2x.png",
+   "width": 172,
+   "height": 52,
+   "x": 685,
+   "y": -4361,
+   "title": "Context menu",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 40,
+    "totalWidth": 1360,
+    "numberOfColumns": 12,
+    "columnWidth": 84,
+    "gutterWidth": 32
+   },
+   "type": "overlay",
+   "overlayByEvent": 0,
+   "overlayPin": 0,
+   "overlayPinHotspot": 5,
+   "overlayPinPage": 0,
+   "overlayOverFixed": false,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": true,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 9,
+      "y": 8,
+      "width": 36,
+      "height": 36
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 49,
+      "y": 8,
+      "width": 36,
+      "height": 36
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 89,
+      "y": 8,
+      "width": 36,
+      "height": 36
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 127,
+      "y": 8,
+      "width": 36,
+      "height": 36
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 3
+    }
+   ]
+  },
+  {
+   "id": "9272D2E5-CC18-41B1-B872-AB16C0353826",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 8,
+   "image": "checkbox.png",
+   "image2x": "checkbox@2x.png",
+   "width": 16,
+   "height": 16,
+   "x": 490,
+   "y": -4359,
+   "title": "Checkbox",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 40,
+    "totalWidth": 1360,
+    "numberOfColumns": 12,
+    "columnWidth": 84,
+    "gutterWidth": 32
+   },
+   "type": "overlay",
+   "overlayByEvent": 0,
+   "overlayPin": 0,
+   "overlayPinHotspot": 3,
+   "overlayPinPage": 0,
+   "overlayOverFixed": false,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": -1.5,
+      "y": -3,
+      "width": 19,
+      "height": 21
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "D8A68FA3-09B6-4AF8-B1BF-7580E4CFBDDC",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 9,
+   "image": "modal_filled.png",
+   "image2x": "modal_filled@2x.png",
+   "width": 417,
+   "height": 659,
+   "x": 942,
+   "y": -4266,
+   "title": "Modal filled",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 40,
+    "totalWidth": 1360,
+    "numberOfColumns": 12,
+    "columnWidth": 84,
+    "gutterWidth": 32
+   },
+   "type": "modal",
+   "isModal": true,
+   "showShadow": 0,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 302,
+      "y": 575,
+      "width": 71,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 11,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 45,
+      "y": 575,
+      "width": 73,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 362,
+      "y": 24,
+      "width": 30,
+      "height": 30
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "3AEBA233-B73F-4FAF-8205-EE01312EC5CC",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 10,
+   "image": "modal_empty.png",
+   "image2x": "modal_empty@2x.png",
+   "width": 417,
+   "height": 516,
+   "x": 485,
+   "y": -4266,
+   "title": "Modal empty",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 40,
+    "totalWidth": 1360,
+    "numberOfColumns": 12,
+    "columnWidth": 84,
+    "gutterWidth": 32
+   },
+   "type": "modal",
+   "isModal": true,
+   "showShadow": 0,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 45,
+      "y": 432,
+      "width": 73,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 362,
+      "y": 24,
+      "width": 30,
+      "height": 30
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 45,
+      "y": 86,
+      "width": 327,
+      "height": 256
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "406E438E-8ED0-443D-A530-8290609906C1",
+   "groupID": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "index": 11,
+   "image": "toaster_overlay.png",
+   "image2x": "toaster_overlay@2x.png",
+   "width": 240,
+   "height": 42,
+   "x": 942,
+   "y": -3557,
+   "title": "Toaster overlay",
+   "transAnimType": 3,
+   "layout": {
+    "offset": 40,
+    "totalWidth": 1360,
+    "numberOfColumns": 12,
+    "columnWidth": 84,
+    "gutterWidth": 32
+   },
+   "type": "overlay",
+   "overlayByEvent": 0,
+   "overlayPin": 1,
+   "overlayPinHotspot": 0,
+   "overlayPinPage": 1,
+   "overlayOverFixed": false,
+   "overlayAlsoFixed": true,
+   "overlayClosePrevOverlay": false,
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 4,
+      "y": 2,
+      "width": 232,
+      "height": 34
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 0
+    }
+   ],
+   "overlayRedirectTargetPage": 0
+  }
+ ],
+ "groups": [
+  {
+   "id": "834A5F03-1EE2-4F5F-AF8C-A22C4EC54371",
+   "name": "Demo dashboard"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 13
+}
