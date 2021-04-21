7fd836ce27584088a98bcfec4fdb6c0e5e3a7a0e
Robert Mazan <rmazan@apple.com>
2021-04-21 11:23:20 +0200

    Added design and extract script

---
+++ src/designTokens.ts
@@ -0,0 +1,190 @@
+/*
+ * Automatically generated file, do not modify manually!
+ */
+
+export const colors = {
+  "grey": "rgba(29, 29, 31, 1)",
+  "fillRed": "rgba(227, 0, 0, 1)",
+  "fillGreyTertiary": "rgba(210, 210, 215, 1)",
+  "greySecondary": "rgba(110, 110, 115, 1)",
+  "blue": "rgba(0, 102, 204, 1)",
+  "greyTertiary": "rgba(210, 210, 215, 1)",
+  "fillGrey": "rgba(29, 29, 31, 1)",
+  "grass": "rgba(0, 128, 9, 1)",
+  "fillGreyQuaternary": "rgba(232, 232, 237, 1)",
+  "fillBlue": "rgba(0, 113, 227, 1)",
+  "fillGreySecondary": "rgba(134, 134, 139, 1)",
+  "fillWhiteTertiary": "rgba(245, 245, 247, 1)"
+};
+
+export const typography = {
+  "h1": {
+    "h1Black": {
+      "fontName": "SFProDisplay-Semibold",
+      "fontSize": 24,
+      "color": "rgba(0, 0, 0, 1)"
+    }
+  },
+  "h2": {
+    "h2Grey1Center": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 19,
+      "color": "rgba(189, 189, 207, 1)"
+    }
+  },
+  "h3": {
+    "h3Medium": {
+      "fontName": "SFProDisplay-Medium",
+      "fontSize": 17,
+      "color": "rgba(0, 0, 0, 1)"
+    },
+    "h3Regular": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 17,
+      "color": "rgba(29, 29, 31, 1)"
+    },
+    "h3RegularBlueCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 17,
+      "color": "rgba(0, 102, 204, 1)"
+    },
+    "h3RegularCenterFillGreyTertiary": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 17,
+      "color": "rgba(210, 210, 215, 1)"
+    },
+    "h3RegularCenterWhiteTertiary": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 17,
+      "color": "rgba(245, 245, 247, 1)"
+    },
+    "h3RegularCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 17,
+      "color": "rgba(0, 0, 0, 1)"
+    }
+  },
+  "body": {
+    "body": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(29, 29, 31, 1)"
+    },
+    "bodyCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(29, 29, 31, 1)"
+    },
+    "bodyGreyTertiaryCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(210, 210, 215, 1)"
+    },
+    "bodyGreySecondaryCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(134, 134, 139, 1)"
+    },
+    "bodyGreySecondaryAlt": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(134, 134, 139, 1)"
+    },
+    "bodyGreyTertiary": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(210, 210, 215, 1)"
+    },
+    "bodyBlue": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 14,
+      "color": "rgba(0, 102, 204, 1)"
+    }
+  },
+  "caption": {
+    "captionGreySecondary": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(134, 134, 139, 1)"
+    },
+    "caption": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(29, 29, 31, 1)"
+    },
+    "captionGreySecondaryCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(134, 134, 139, 1)"
+    },
+    "captionBlue": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(0, 102, 204, 1)"
+    },
+    "captionWhite": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(255, 255, 255, 1)"
+    },
+    "captionCenter": {
+      "fontName": "SFProDisplay-Regular",
+      "fontSize": 12,
+      "color": "rgba(0, 0, 0, 1)"
+    }
+  },
+  "captionSmall": {
+    "fontName": "SFProDisplay-Regular",
+    "fontSize": 10,
+    "color": "rgba(103, 103, 103, 1)"
+  },
+  "glyph": {
+    "linkChevronCaptionMediumWhite": {
+      "fontName": "SFProDisplay-Medium",
+      "fontSize": 7.5,
+      "color": "rgba(255, 255, 255, 1)"
+    },
+    "linkChevronCaptionMediumBlue": {
+      "fontName": "SFProDisplay-Medium",
+      "fontSize": 7.5,
+      "color": "rgba(0, 102, 204, 1)"
+    }
+  }
+};
+
+export const layerStyles = {
+  "fillRed": {
+    "fillColor": "rgba(227, 0, 0, 1)"
+  },
+  "textfieldDefault": {
+    "fillColor": "rgba(255, 255, 255, 1)",
+    "borderColor": "rgba(210, 210, 215, 1)"
+  },
+  "fillGrey": {
+    "fillColor": "rgba(29, 29, 31, 1)"
+  },
+  "grass": {
+    "fillColor": "rgba(0, 128, 9, 1)"
+  },
+  "fillGreyQuaternary": {
+    "fillColor": "rgba(232, 232, 237, 1)"
+  },
+  "textfieldActive": {
+    "fillColor": "rgba(255, 255, 255, 1)",
+    "borderColor": "rgba(0, 113, 227, 1)"
+  },
+  "fillGreyTertiary": {
+    "fillColor": "rgba(210, 210, 215, 1)"
+  },
+  "textfieldHover": {
+    "fillColor": "rgba(255, 255, 255, 1)",
+    "borderColor": "rgba(134, 134, 139, 1)"
+  },
+  "fillWhiteTertiary": {
+    "fillColor": "rgba(245, 245, 247, 1)",
+    "borderColor": "rgba(210, 210, 215, 1)"
+  },
+  "fillGrass": {
+    "fillColor": "rgba(3, 161, 14, 1)"
+  }
+};
